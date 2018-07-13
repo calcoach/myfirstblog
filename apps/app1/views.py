eb636b4c9a20583f8d205b1b7af5055e58d4fbcc
Alejandro Santiago <ajsantiago@uninorte.edu.co>
2018-07-13 14:35:22 -0500

    s

--- apps/app1/views.py
+++ apps/app1/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
 import requests
+
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
