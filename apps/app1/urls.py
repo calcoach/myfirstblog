d1522a31ac04e4053bced9ded2ef8878edefc5d1
Alejandro Santiago <alejocabaypipe@gmail.com>
2021-01-22 02:02:26 -0500

    Privacy policies page added

--- apps/app1/urls.py
+++ apps/app1/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from apps.app1.views import index
+from apps.app1.views import privacypolicy
 
 urlpatterns= [
     url(r'^', index),
+    url(r'^', privacypolicy, name = 'privacypolicy'),
 
 ]
--- apps/app1/views.py
+++ apps/app1/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def privacypolicy(request):
+    return render(request, 'privacypolicy.html')
